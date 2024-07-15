25030e0f98108ef664b9db7626acb0ae4f2f0a73
AbullahUsama087 <abdullah.usama087@gmail.com>
2024-07-15 04:30:23 +0300

    the main Routes of the Project

---
+++ Src/Modules/index.routes.js
@@ -0,0 +1,7 @@
+import userRouter from "./User/user.routes.js";
+
+import taskRouter from "./Task/task.routes.js";
+
+import categoryRouter from "./Category/category.routes.js";
+
+export { userRouter, taskRouter, categoryRouter };
